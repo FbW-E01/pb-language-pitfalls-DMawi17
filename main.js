3e046c691d59fe1cb17f427d3450622dc6475521
Dagmawi <asegda17@gmail.com>
2021-04-01 13:23:05 +0200

    pitfalls

---
+++ main.js
@@ -0,0 +1,16 @@
+// Q1
+
+// NaN stands for Not a Number.
+console.log(isNaN('hello')); // NaN
+console.log(isNaN(3)); // Not a NaN
+console.log(typeof NaN); // number
+
+// Q2
+console.log((0.1 * 0.2).toFixed(2));
+
+// Q3
+console.log(Infinity / 0); // Infinity
+console.log(Infinity / Infinity); // NaN
+console.log(1 / 0); // Infinity
+
+
